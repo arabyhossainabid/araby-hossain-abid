eba61911ec243e95b62eaccfa4aa8b7e3d0201ba
arabyhossainabid <arabyhossainabid@gmail.com>
2025-12-04 15:30:11 +0600

    feat: Add `Worked` component to display professional experience.

--- src/components/projects-page-ui/work.tsx
+++ src/components/projects-page-ui/work.tsx
@@ -75,7 +75,7 @@ function Worked() {
                         {exp.role}
                       </div>
                       <p className="text-gray-400 leading-relaxed">
-                        {exp.description}
+                        {exp.description.replace(/'/g, "&apos;")}
                       </p>
                     </div>
                     <button
